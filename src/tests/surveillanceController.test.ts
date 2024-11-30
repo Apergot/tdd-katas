52d6a9ec681d5f04f5d78eec9920175e730c1ee2
aperdomo <apergot95@gmail.com>
2024-11-30 10:24:56 +0000

    :hammer: use jest mock to set sensor state when needed

--- src/tests/surveillanceController.test.ts
+++ src/tests/surveillanceController.test.ts
@@ -1,38 +1,40 @@
 import { MotionSensor, SurveillanceController, VideoRecorder } from '../core/surveillanceController';
 
 describe('The Surveillance Controller', () => {
+	let sensor: MotionSensor;
+	let recorder: VideoRecorder;
+	let controller: SurveillanceController;
+
+	beforeEach(() => {
+		sensor = new FakeSensor();
+		recorder = new FakeRecorder();
+		controller = new SurveillanceController(sensor, recorder);
+	});
+
 	it('should ask the recorder to stop recording when the sensor detects no motion', () => {
-		const sensor = new StubSensorDetectingNoMotion();
-		const recorder = new FakeRecorder();
-		const controller = new SurveillanceController(sensor, recorder);
 		const spyRecorder = jest.spyOn(recorder, 'stopRecording');
 
 		controller.recordMotion();
 
 		expect(spyRecorder).toHaveBeenCalled();
+		expect(sensor.isDetectingMotion()).toBe(false);
 	});
 
 	it('should ask the recorder to start recording when the sensor detects motion', () => {
-		const sensor = new StubSensorDetectingMotion();
-		const recorder = new FakeRecorder();
-		const controller = new SurveillanceController(sensor, recorder);
 		const spyRecorder = jest.spyOn(recorder, 'startRecording');
+		const stubSensor = jest.spyOn(sensor, 'isDetectingMotion');
+		stubSensor.mockImplementation(() => true);
 
 		controller.recordMotion();
 
 		expect(spyRecorder).toHaveBeenCalled();
+		expect(sensor.isDetectingMotion()).toBe(true);
 	});
 });
 
-class StubSensorDetectingNoMotion implements MotionSensor {
+class FakeSensor implements MotionSensor {
 	isDetectingMotion(): boolean {
 		return false;
-	}
-}
-
-class StubSensorDetectingMotion implements MotionSensor {
-	isDetectingMotion(): boolean {
-		return true;
 	}
 }
 
